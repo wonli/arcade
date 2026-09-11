86fdd0726aee02ca19192b07d8a46eeda9999f9d
ideaa <ideaa@qq.com>
2026-09-11 10:37:46 +0800

    feat: add in-memory room manager

---
+++ room/manager.go
@@ -0,0 +1,49 @@
+package room
+
+import (
+	"crypto/rand"
+	"errors"
+	"sync"
+)
+
+const alphabet = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+
+type Manager struct {
+	rooms sync.Map
+}
+
+func NewManager() *Manager { return &Manager{} }
+
+func (m *Manager) Create(gameName string) (*Room, error) {
+	for range 10 {
+		id, err := roomID(6)
+		if err != nil {
+			return nil, err
+		}
+		r := New(id, gameName)
+		if _, loaded := m.rooms.LoadOrStore(id, r); !loaded {
+			return r, nil
+		}
+	}
+	return nil, errors.New("could not allocate room id")
+}
+
+func (m *Manager) Get(id string) (*Room, bool) {
+	v, ok := m.rooms.Load(id)
+	if !ok {
+		return nil, false
+	}
+	return v.(*Room), true
+}
+
+func roomID(length int) (string, error) {
+	buf := make([]byte, length)
+	random := make([]byte, length)
+	if _, err := rand.Read(random); err != nil {
+		return "", err
+	}
+	for i := range buf {
+		buf[i] = alphabet[int(random[i])%len(alphabet)]
+	}
+	return string(buf), nil
+}
