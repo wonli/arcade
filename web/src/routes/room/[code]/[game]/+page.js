8120791c98a8eeabe8a04c5f47882a61d5904930
ideaa <ideaa@qq.com>
2026-09-11 14:34:32 +0800

    feat: pass room mode into creation page

--- web/src/routes/room/[code]/[game]/+page.js
+++ web/src/routes/room/[code]/[game]/+page.js
@@ -1,9 +1,10 @@
 export const ssr = false
 export const prerender = false
 
-export function load({ params }) {
+export function load({ params, url }) {
   return {
     code: params.code,
     game: params.game,
+    players: Number(url.searchParams.get('players')) === 1 ? 1 : 2,
   }
 }
