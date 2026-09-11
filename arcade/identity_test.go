91163cdf5d6ff92fb2335dd8bbf29c8865acdc04
ideaa <ideaa@qq.com>
2026-09-11 14:23:19 +0800

    test: define per-tab arcade seat identity

--- arcade/identity_test.go
+++ arcade/identity_test.go
@@ -2,14 +2,18 @@ package arcade
 
 import "testing"
 
-func TestGuestIdentitySeparatesPlayerAndSession(t *testing.T) {
-	uid := GuestUID("player-123")
+func TestGuestIdentitySeparatesPlayerSessionAndSeat(t *testing.T) {
+	uid := GuestUID("player-123", "session-456")
 	appID := GuestAppID("session-456")
+	seatID := GuestSeatID("session-456")
 
-	if uid != "arcade:player-123" {
+	if uid != "arcade:player-123:session-456" {
 		t.Fatalf("uid = %q", uid)
 	}
 	if appID != "web:session-456" {
 		t.Fatalf("appID = %q", appID)
+	}
+	if seatID != "session-456" {
+		t.Fatalf("seatID = %q", seatID)
 	}
 }
