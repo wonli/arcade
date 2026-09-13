0ff69541e1c2ab6be8be32e3de60bb40cd7ee007
ideaa <ideaa@qq.com>
2026-09-13 12:05:40 +0800

    fix: resolve chess assets from repository root

--- web/src/lib/games/chess/pieces.js
+++ web/src/lib/games/chess/pieces.js
@@ -1,9 +1,9 @@
-import pawn from '../../../../../assets/chess/pieces/pawn.svg'
-import knight from '../../../../../assets/chess/pieces/knight.svg'
-import bishop from '../../../../../assets/chess/pieces/bishop.svg'
-import rook from '../../../../../assets/chess/pieces/rook.svg'
-import queen from '../../../../../assets/chess/pieces/queen.svg'
-import king from '../../../../../assets/chess/pieces/king.svg'
+import pawn from '../../../../assets/chess/pieces/pawn.svg'
+import knight from '../../../../assets/chess/pieces/knight.svg'
+import bishop from '../../../../assets/chess/pieces/bishop.svg'
+import rook from '../../../../assets/chess/pieces/rook.svg'
+import queen from '../../../../assets/chess/pieces/queen.svg'
+import king from '../../../../assets/chess/pieces/king.svg'
 
 const pieceAssets = ['', pawn, knight, bishop, rook, queen, king]
 
