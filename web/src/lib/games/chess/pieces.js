ad087a6d4f2ebc70da1387019dca783809dd54f1
ideaa <ideaa@qq.com>
2026-09-13 12:03:52 +0800

    feat: import chess pieces from source assets

--- web/src/lib/games/chess/pieces.js
+++ web/src/lib/games/chess/pieces.js
@@ -1,6 +1,12 @@
-const pieceNames = ['', 'pawn', 'knight', 'bishop', 'rook', 'queen', 'king']
+import pawn from '../../../../../assets/chess/pieces/pawn.svg'
+import knight from '../../../../../assets/chess/pieces/knight.svg'
+import bishop from '../../../../../assets/chess/pieces/bishop.svg'
+import rook from '../../../../../assets/chess/pieces/rook.svg'
+import queen from '../../../../../assets/chess/pieces/queen.svg'
+import king from '../../../../../assets/chess/pieces/king.svg'
+
+const pieceAssets = ['', pawn, knight, bishop, rook, queen, king]
 
 export function chessPieceAsset(piece) {
-  const name = pieceNames[Math.abs(piece)]
-  return name ? `/assets/chess/pieces/${name}.svg` : ''
+  return pieceAssets[Math.abs(piece)] ?? ''
 }
