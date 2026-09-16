dcee1f409e65bed6157f7a4583fa5e50a9843fc5
ideaa <ideaa@qq.com>
2026-09-16 12:38:35 +0800

    refactor(dungeon): stabilize loot gameplay capability

--- web/src/lib/games/dungeon/gameplay-capabilities.js
+++ web/src/lib/games/dungeon/gameplay-capabilities.js
@@ -1,4 +1,5 @@
 import { ensureDungeonCombatRuntime } from './combat-runtime.js'
+import { ensureDungeonLootRuntime } from './loot-runtime.js'
 
 function objectOwner(value, label) {
   if (value == null) return {}
@@ -13,15 +14,17 @@ export function ensureDungeonCapabilities(scene) {
   scene.dungeon = dungeon
   dungeon.combat = ensureDungeonCombatRuntime(scene)
 
-  const loot = objectOwner(dungeon.loot, 'scene.dungeon.loot')
-  dungeon.loot = loot
+  const hadLoot = dungeon.loot && typeof dungeon.loot === 'object' ? dungeon.loot : null
+  const hadPickup = typeof hadLoot?.pickup === 'function'
+  const hadOpenChest = typeof hadLoot?.openChest === 'function'
+  dungeon.loot = ensureDungeonLootRuntime(scene)
 
-  if (typeof loot.pickup !== 'function' && typeof scene.__dungeonPickupRuntime?.pickupById === 'function') {
-    loot.pickup = (player, dropId) => scene.__dungeonPickupRuntime?.pickupById?.(player, dropId)
+  if (!hadPickup && typeof scene.__dungeonPickupRuntime?.pickupById === 'function') {
+    dungeon.loot.setPickupOwner((player, dropId) => scene.__dungeonPickupRuntime?.pickupById?.(player, dropId))
   }
 
-  if (typeof loot.openChest !== 'function' && typeof scene.__dungeonSpatial?.openChestById === 'function') {
-    loot.openChest = (player, chestId) => scene.__dungeonSpatial?.openChestById?.(player, chestId)
+  if (!hadOpenChest && typeof scene.__dungeonSpatial?.openChestById === 'function') {
+    dungeon.loot.setOpenChestOwner((player, chestId) => scene.__dungeonSpatial?.openChestById?.(player, chestId))
   }
 
   return dungeon
