5c4f0e720af93f0b5dd1b20d882f7c6bb19476a7
ideaa <ideaa@qq.com>
2026-09-17 17:49:55 +0800

    test: define replay host ownership contract

---
+++ arcade/replay_test.go
@@ -0,0 +1,38 @@
+package arcade
+
+import (
+	"testing"
+
+	"github.com/wonli/arcade/game"
+)
+
+func TestReplayHostRequiresStartedMatchingRoomHost(t *testing.T) {
+	service := NewService()
+	roomValue, err := service.Create("gomoku", 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	host := game.PlayerID("host")
+	guest := game.PlayerID("guest")
+	if err := service.Join(roomValue.ID, host, "Host"); err != nil {
+		t.Fatal(err)
+	}
+	if service.ReplayHost(roomValue.ID, host, "gomoku") {
+		t.Fatal("waiting room must not grant replay host")
+	}
+	if err := service.Join(roomValue.ID, guest, "Guest"); err != nil {
+		t.Fatal(err)
+	}
+	if !service.ReplayHost(roomValue.ID, host, "gomoku") {
+		t.Fatal("started room host should be replay host")
+	}
+	if service.ReplayHost(roomValue.ID, guest, "gomoku") {
+		t.Fatal("non-host player must not be replay host")
+	}
+	if service.ReplayHost(roomValue.ID, host, "chess") {
+		t.Fatal("game mismatch must not be accepted")
+	}
+	if service.ReplayHost("missing", host, "gomoku") {
+		t.Fatal("missing room must not be accepted")
+	}
+}
