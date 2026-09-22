38b01a8b8f53738a8d6bdd8c1e74de683ab06427
ideaa <ideaa@qq.com>
2026-09-22 14:33:11 +0800

    test: require police thief svg pieces

--- web/src/lib/games/policethief/board-labels.test.js
+++ web/src/lib/games/policethief/board-labels.test.js
@@ -7,6 +7,13 @@ const source = await readFile(new URL('./PoliceThiefBoard.svelte', import.meta.u
 test('police thief pieces render readable role names', () => {
   assert.match(source, /thiefLabel/)
   assert.match(source, /policeLabel/)
-  assert.match(source, /thief-piece[^>]*>\{thiefLabel\}</)
-  assert.match(source, /police-piece[^>]*>\{policeLabel\}</)
+  assert.match(source, /thief-piece/)
+  assert.match(source, /police-piece/)
+})
+
+test('police thief pieces use the committed svg assets', () => {
+  assert.match(source, /game\/policethief\/assets\/p\.svg/)
+  assert.match(source, /game\/policethief\/assets\/t\.svg/)
+  assert.match(source, /<img[^>]+src=\{policeIcon\}/)
+  assert.match(source, /<img[^>]+src=\{thiefIcon\}/)
 })
