f9740c95838668884e9f9a36113528b5c1a5838a
ideaa <ideaa@qq.com>
2026-09-24 15:11:14 +0800

    feat: register tank launcher and replay

--- web/src/lib/games/registry.js
+++ web/src/lib/games/registry.js
@@ -2,6 +2,7 @@ import { launcher as gomokuLauncher } from './gomoku/launcher.js'
 import { launcher as chessLauncher } from './chess/launcher.js'
 import { launcher as xiangqiLauncher } from './xiangqi/launcher.js'
 import { launcher as tetrisLauncher } from './tetris/launcher.js'
+import { launcher as tankLauncher } from './tank/launcher.js'
 import { launcher as snakeLauncher } from './snake/launcher.js'
 import { launcher as drawguessLauncher } from './drawguess/launcher.js'
 import { launcher as dungeonLauncher } from './dungeon/launcher.js'
@@ -10,6 +11,7 @@ import { replay as gomokuReplay } from './gomoku/replay.js'
 import { replay as chessReplay } from './chess/replay.js'
 import { replay as xiangqiReplay } from './xiangqi/replay.js'
 import { replay as tetrisReplay } from './tetris/replay.js'
+import { replay as tankReplay } from './tank/replay.js'
 import { replay as snakeReplay } from './snake/replay.js'
 import { replay as drawguessReplay } from './drawguess/replay.js'
 import { replay as dungeonReplay } from './dungeon/replay.js'
@@ -27,6 +29,7 @@ export const GAME_ENTRIES = Object.freeze([
   Object.freeze({ id: 'chess', launcher: chessLauncher, replay: chessReplay }),
   Object.freeze({ id: 'xiangqi', launcher: xiangqiLauncher, replay: xiangqiReplay }),
   Object.freeze({ id: 'tetris', launcher: tetrisLauncher, replay: tetrisReplay }),
+  Object.freeze({ id: 'tank', launcher: tankLauncher, replay: tankReplay }),
   Object.freeze({ id: 'snake', launcher: snakeLauncher, replay: snakeReplay }),
   Object.freeze({ id: 'drawguess', launcher: drawguessLauncher, replay: drawguessReplay }),
   Object.freeze({ id: 'dungeon', launcher: dungeonLauncher, replay: dungeonReplay }),
